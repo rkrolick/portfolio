ded3325c383e526e04fd8428643cd4228c98ee01
Ryan Krolick <rkrolick@gmail.com>
2016-05-09 19:39:02 -0400

    testing php email

--- mailer.php
+++ mailer.php
@@ -4,5 +4,6 @@ $phone = $_POST["phone"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 $body = "Name: {$name}\nPhone: {$phone}\nEmail: {$email}\n{$message}";
-echo $body;
+mail("rkrolick@gmail.com", "Website Email", $body);
+echo "Email Sent!"
 ?>
